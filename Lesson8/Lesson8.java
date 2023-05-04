33c4a5eab8a7e89855972d705ee6822843579897
Dice1111 <kingdice1111@gmail.com>
2023-05-04 15:56:15 +0800

    Lesson8

--- Lesson8/Lesson8.java
+++ Lesson8/Lesson8.java
@@ -3,8 +3,21 @@ package Lesson8;
 public class Lesson8 {
     public static void main(String[] args) {
         printToTen(5);
+        System.out.println(factorial(5));
     }
 
+    public static int fibo(int n){
+        if(n<=1){
+            return n;
+        }else{
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
+
+    public static int factorial(int n){
+        if(n==0) return 1;
+        else return factorial(n-1)*n;
+    }
     public static void printToTen(int n){
         if(n!=11){
             System.out.println(n);
